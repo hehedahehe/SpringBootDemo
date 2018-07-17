91c590c2e4457b8cb97aad08b2478de6a15ed759
liruibo <liruibo@guazi.com>
2018-07-18 00:57:43 +0800

    +counter example

---
+++ multithread/src/main/java/com/cesuokanc/multithread/CounterSyn.java
@@ -0,0 +1,47 @@
+package com.cesuokanc.multithread;
+
+/**
+ * @author liruibo
+ * @date 2018/7/18
+ */
+public class CounterSyn {
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public int value() {
+        return c;
+    }
+
+    public static void main(String[] args)throws InterruptedException{
+        long begin = System.currentTimeMillis();
+
+        CounterSyn counter = new CounterSyn();
+        int times = 1000000;
+        Thread t1 = new Thread(()->{
+           for(int i=0;i<times;i++){
+               counter.increment();
+           }
+        });
+        Thread t2 = new Thread(()->{
+            for(int i=0;i<times;i++){
+                counter.decrement();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        long end = System.currentTimeMillis();
+        System.out.println((end-begin)+"====>"+counter.value());
+
+    }
+}
